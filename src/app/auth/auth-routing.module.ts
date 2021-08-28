6f566b77eb78c6dabf2db60258c1f3ef69f4fa08
lethanhtuyen1008 <53989518+lethanhtuyen1008@users.noreply.github.com>
2021-08-28 22:02:41 +0700

    test

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './auth.component';
 import { NoAuthGuard } from './no-auth-guard.service';
 
-
 const routes: Routes = [
   {
     path: 'login',
