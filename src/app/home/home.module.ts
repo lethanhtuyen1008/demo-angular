f3b30e24e19cd64b610794a115af1d832c14d0d5
lethanhtuyen1008 <53989518+lethanhtuyen1008@users.noreply.github.com>
2021-08-30 17:52:26 +0700

    Code

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { CustomButtonModule } from '../shared/custom-button/custom-button.module
 import { HomeAuthResolver } from './home-auth-resolver.service';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [HomeRoutingModule, CustomButtonModule],
+  imports: [HomeRoutingModule, CustomButtonModule, CommonModule],
   declarations: [HomeComponent],
   providers: [HomeAuthResolver],
 })
