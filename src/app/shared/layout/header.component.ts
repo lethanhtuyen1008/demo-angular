f4517c9233af47ec500b56e893177d036c60eef5
lethanhtuyen1008 <53989518+lethanhtuyen1008@users.noreply.github.com>
2021-08-31 20:11:24 +0700

    Design menu

--- src/app/shared/layout/header.component.ts
+++ src/app/shared/layout/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User, UserService } from '../../core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-layout-header',
@@ -9,10 +10,28 @@ import { User, UserService } from '../../core';
 export class HeaderComponent implements OnInit {
   constructor(private userService: UserService) {}
   currentUser: User;
+  classOpen = 'show';
+  classClose = 'hidden';
 
   ngOnInit() {
     this.userService.currentUser.subscribe((userData) => {
       this.currentUser = userData;
     });
+  }
+
+  onOpenMenu() {
+    $(".navigation").addClass("active");
+    this.classOpen = 'hidden';
+    this.classClose = 'show';
+    $("#menu").addClass("active-menu");
+    $("#menu").removeClass("close-menu");
+  }
+
+  onCloseMenu() {
+    $(".navigation").removeClass("active");
+    $("#menu").removeClass("active-menu");
+    $("#menu").addClass("close-menu");
+    this.classOpen = 'show';
+    this.classClose = 'hidden';
   }
 }
