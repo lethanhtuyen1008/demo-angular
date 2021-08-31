2ee95a6c50bd1d6635f69b07e35810ee953a8b46
lethanhtuyen1008 <53989518+lethanhtuyen1008@users.noreply.github.com>
2021-08-31 14:00:05 +0700

    Code

--- src/app/shared/layout/header.component.ts
+++ src/app/shared/layout/header.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
 import { User, UserService } from '../../core';
 
 @Component({
   selector: 'app-layout-header',
   templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
   constructor(private userService: UserService) {}
-
   currentUser: User;
 
   ngOnInit() {
