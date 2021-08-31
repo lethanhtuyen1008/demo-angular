0550a3b1ac617a0a224c3860b6d22019cf7e03cd
lethanhtuyen1008 <53989518+lethanhtuyen1008@users.noreply.github.com>
2021-08-31 21:41:03 +0700

    Code

--- src/app/shared/layout/header.component.ts
+++ src/app/shared/layout/header.component.ts
@@ -1,22 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { User, UserService } from '../../core';
 import * as $ from 'jquery';
-
+  
 @Component({
   selector: 'app-layout-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService,  ) {}
   currentUser: User;
   classOpen = 'show';
   classClose = 'hidden';
+  urlActive = '';
 
   ngOnInit() {
     this.userService.currentUser.subscribe((userData) => {
       this.currentUser = userData;
     });
+
+    this.urlActive = (window.location.href.split('/')[3]);
+    
+    if (localStorage.getItem('isOpen') === 'true') {
+      $('.navigation').addClass('active');
+      this.classOpen = 'hidden';
+      this.classClose = 'show';
+      $('#menu').addClass('active-menu');
+      $('#menu').removeClass('close-menu');
+    }
   }
 
   onOpenMenu() {
@@ -25,6 +36,7 @@ export class HeaderComponent implements OnInit {
     this.classClose = 'show';
     $('#menu').addClass('active-menu');
     $('#menu').removeClass('close-menu');
+    localStorage.setItem('isOpen', 'true');
   }
 
   onCloseMenu() {
@@ -33,5 +45,6 @@ export class HeaderComponent implements OnInit {
     $('#menu').addClass('close-menu');
     this.classOpen = 'show';
     this.classClose = 'hidden';
+    localStorage.setItem('isOpen', 'false');
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://conduit.productionready.io/api'
+  api_url: 'https://5da6f06a127ab80014c1da65.mockapi.io/api'
 };
